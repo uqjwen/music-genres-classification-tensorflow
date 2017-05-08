3fde569ac072fc7932dd2091a231f54f801f66fa
uqjwen <j.wen@uq.edu.au>
2017-05-08 15:59:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+audiopath = './raw/'
+specpath = './spec/'
+slicepath = './slice/'
+
+
+
+sliceSize = 128
+
+filesPerGenre = 10000
+
+pixelPerSecond = 50
+	
+batchSize = 128
+
+numEpoches = 200
+
+checkEvery = 100
